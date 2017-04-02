4eee1d141fee78c7ddb0cc6c0265e69a136cf31d
Jack Storch <jstorch33@gmail.com>
2017-04-01 23:58:35 -0700

    some work

---
+++ MessagesExtension/ExpandedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ExpandedViewController.swift
+//  SwitchSMS
+//
+//  Created by Jack Storch on 4/1/17.
+//  Copyright © 2017 John Storch. All rights reserved.
+//
+
+import UIKit
+
+class ExpandedViewController: UICollectionViewController {
+
+     static let storyboardIdentifier = "ExpandedViewController"
+    
+    
+
+
+}
+
+protocol ExpandedViewControllerDelegate: class {
+    
+}
